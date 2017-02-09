2f2e4775498fe970c1c4c7b7b5ca5fc26e681364
Stephen Chen <sih-han.chen@sjsu.edu>
2017-02-09 02:51:58 -0800

    debugging

--- server/notifier/notifyServer.go
+++ server/notifier/notifyServer.go
@@ -15,6 +15,8 @@ var my_channel = "All_Bus_Info"
 var db_addr = "54.191.90.246:27017"
 var BusStopMap = make(map[Coordinate]string)
 
+var pubnub = messaging.NewPubnub(my_pubkey, my_subkey, "", "", false, "")
+
 type SensorSignal struct {
 	SignalID  string  `json:"signal_id"`
 	SensorID  string  `json:"sensor_id"`
@@ -54,8 +56,19 @@ func subscribeSensorInfo() {
 				case float64:
 					fmt.Println(msg[1].(string))
 				case []interface{}:
-					fmt.Printf("Received message '%s' on channel '%s'\n", m[0], msg[2])
-					//return
+					fmt.Printf("Received message '%s' on channel '%s'\n", m[0], msg[2]) //m[0]:the JSON data. m[2]=channel ID
+					var signal SensorSignal
+					err := json.Unmarshal([]byte(m[0].(string)), &signal)
+					if err == nil {
+						fmt.Println("long:", signal.Long)
+						fmt.Println("lat:", signal.Lat)
+						chID := isHitBusStop(signal.Long, signal.Lat)
+						if chID != "" {
+							notifyChannel(m[0].(string), chID)
+						}
+					} else {
+						fmt.Println("Unmarshal failed! =>", err)
+					}
 				default:
 					panic(fmt.Sprintf("Unknown type: %T", m))
 				}
@@ -69,48 +82,83 @@ func subscribeSensorInfo() {
 	}()
 }
 
-func initBusStopChannel() {
-	BusStopMap[Coordinate{Long: -122.14292, Lat: 37.44198}] = "Bus_Stop_A"
-	BusStopMap[Coordinate{Long: -122.10125, Lat: 37.42798}] = "Bus_Stop_B"
-	BusStopMap[Coordinate{Long: -121.89964, Lat: 37.43222}] = "Bus_Stop_C"
-
-	/*
-		for k, v := range BusStopMap {
-		fmt.Println("Key:", k.Long, ",", k.Lat, "Value:", v)
-		}*/
-
-	x := -122.14292
-	y := 37.44198
-
-	_, ok := BusStopMap[Coordinate{x, y}]
-	if ok {
-		fmt.Println("Arrived!", BusStopMap[Coordinate{x, y}])
-
-		pubnub := messaging.NewPubnub(my_pubkey, my_subkey, "", "", false, "")
-		fmt.Println("PubNub SDK for go;", messaging.VersionInfo())
-		successChannel := make(chan []byte)
-		errorChannel := make(chan []byte)
-
-		j := "Ding Don!"
+func notifyChannel(data string, channelID string) {
+	successChannel := make(chan []byte)
+	errorChannel := make(chan []byte)
+	fmt.Println("Enter notifyChannel -1")
+	go func() {
 		for {
-			time.Sleep(10000 * time.Millisecond)
-			go pubnub.Publish(BusStopMap[Coordinate{x, y}], string(j), successChannel, errorChannel)
+			fmt.Println("Enter notifyChannel -2")
+			go pubnub.Publish(channelID, data, successChannel, errorChannel)
 
+			fmt.Println("Enter notifyChannel -3")
 			select {
 			case response := <-successChannel:
 				fmt.Println(string(response))
-				fmt.Println("Sent Message " + string(j))
+				fmt.Println("Sent Message " + data)
 			case err := <-errorChannel:
 				fmt.Println(string(err))
 			case <-messaging.Timeout():
 				fmt.Println("Publish() timeout")
 			}
 		}
+	}()
+}
+
+func isHitBusStop(Long float64, Lat float64) string {
+
+	_, ok := BusStopMap[Coordinate{Long, Lat}] // if this coordinate founds in the hashmap
+	if ok {
+		fmt.Println("Arrived!", BusStopMap[Coordinate{Long, Lat}])
+		return BusStopMap[Coordinate{Long, Lat}]
+	}
+	return ""
+
+}
+
+func initBusStopChannel() {
+	BusStopMap[Coordinate{Long: -122.14292, Lat: 37.44198}] = "Bus_Stop_A"
+	BusStopMap[Coordinate{Long: -122.10125, Lat: 37.42798}] = "Bus_Stop_B"
+	BusStopMap[Coordinate{Long: -121.89964, Lat: 37.43222}] = "Bus_Stop_C"
+
+	/* // For Test purpose only. It can be remove.
+	for k, v := range BusStopMap {
+	fmt.Println("Key:", k.Long, ",", k.Lat, "Value:", v)
+	}*/
+
+	x := -122.14292
+	y := 37.44198
+
+	_, ok := BusStopMap[Coordinate{x, y}] // if this coordinate founds in the hashmap
+	if ok {
+		fmt.Println("Arrived!", BusStopMap[Coordinate{x, y}])
+
+		successChannel := make(chan []byte)
+		errorChannel := make(chan []byte)
+
+		j := "Ding Don!"
+		go func() {
+			for {
+				time.Sleep(10000 * time.Millisecond)
+				go pubnub.Publish(BusStopMap[Coordinate{x, y}], string(j), successChannel, errorChannel)
+
+				select {
+				case response := <-successChannel:
+					fmt.Println(string(response))
+					fmt.Println("Sent Message " + string(j))
+				case err := <-errorChannel:
+					fmt.Println(string(err))
+				case <-messaging.Timeout():
+					fmt.Println("Publish() timeout")
+				}
+			}
+		}()
 	}
 }
 
 func main() {
+	fmt.Println("PubNub SDK for go;", messaging.VersionInfo())
 	initBusStopChannel()
-	//subscribeSensorInfo()
+	subscribeSensorInfo()
 	http.ListenAndServe(":8080", nil)
 }
