b5a86b2206c97d0ef68fd7770a1f1f710dd08549
Wayne Huang <wayne@Waynes-MacBook-Pro.local>
2017-02-11 21:28:54 -0800

    add partial PubNub implementation (draft ver.)

--- iOSSmartBus/iOSSmartBus/ViewController.m
+++ iOSSmartBus/iOSSmartBus/ViewController.m
@@ -8,13 +8,18 @@
 
 #import "ViewController.h"
 #import <AudioToolbox/AudioServices.h>
+#import <PubNub/PubNub.h>
 
-@interface ViewController (){
+@interface ViewController () <PNObjectEventListener> {
     //Picker View
     UIPickerView *picker;
     
     NSArray *stopArray;
+    NSString *beginStop;
+    NSString *endStop;
 }
+
+@property (nonatomic, strong) PubNub *client;
 
 @end
 
@@ -37,22 +42,46 @@
     picker.delegate = self;
     [picker selectRow:0 inComponent:0 animated:YES];
     
-    stopArray = [[NSArray alloc]initWithObjects:@"A", @"B", @"C", @"D"
-                , @"E", @"F", @"G", @"H"
-                , @"H", @"J", nil];
+    stopArray = [[NSArray alloc]initWithObjects:@"A", @"B", @"C", nil];
     
-    self.txtDestination.inputView = picker;
+    
+    
+    // hard code for those info
+    [self.btnRouteA setTitle:@"Folsome - Fremon" forState:UIControlStateNormal];
+    [self.btnRouteB setTitle:@"Colma - Fremon" forState:UIControlStateNormal];
+    [self.btnRouteC setTitle:@"Colma - Orinda" forState:UIControlStateNormal];
+    [self.lableRouteA setText:@"5 min"];
+    [self.lableRouteB setText:@"10 min"];
+    [self.lableRouteC setText:@"16 min"];
+    [self.viewRouteA setHidden:YES];
+    [self.viewRouteB setHidden:YES];
+    [self.viewRouteC setHidden:YES];
+    
+    [self initPubNub];
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (void)initPubNub
+{
+    // Initialize and configure PubNub client instance
+    PNConfiguration *configuration = [PNConfiguration configurationWithPublishKey:@"pub-c-275d4bd0-6556-4125-905c-a9f365a86a37"
+                                                                     subscribeKey:@"sub-c-ac319e2e-ee4c-11e6-b325-02ee2ddab7fe"];
+    self.client = [PubNub clientWithConfiguration:configuration];
+    [self.client addListener:self];
+}
+
 #pragma mark - Picker View Delegate
 - (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
     return [stopArray objectAtIndex:row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    self.txtDestination.text = [stopArray objectAtIndex:row];
 }
 
 #pragma mark - Picker View Data source
@@ -67,14 +96,61 @@
 #pragma mark - Gesture Responder
 - (void)tapGesture:(UITapGestureRecognizer *)gestureRecognizer {
     [self.view endEditing:YES];
+    if (![self.txtDestination.text isEqualToString:@""])
+    {
+        beginStop = @"Bus_Stop_A";
+        
+        // ready to appear route options for buttons
+        [self.viewRouteA setHidden:NO];
+        [self.viewRouteB setHidden:NO];
+        [self.viewRouteC setHidden:NO];
+    }
 }
 
 #pragma mark - Button Click
-- (IBAction)btnDestinationClicked:(id)sender {
-    //subcribe to PubNub
+- (IBAction)btnDestinationClicked:(id)sender
+{
+    // unsubscribe
+    [self.client unsubscribeFromAll];
+}
+
+- (IBAction)btnRouteAClicked:(id)sender {
+    [self.client subscribeToChannels:@[@"Bus_Stop_A"] withPresence:YES];
+    endStop = @"Bus_Stop_A";
     
-    //Virbration
-    [self vibratePhone];
+    NSString *fff = @"{\"action\":7}";
+//    NSDictionary *fff = @{@"action":@"7"};
+    
+//    NSError * err;
+//    NSData * jsonData = [NSJSONSerialization  dataWithJSONObject:fff options:0 error:&err];
+//    NSString * myString = [[NSString alloc] initWithData:jsonData   encoding:NSUTF8StringEncoding];
+    
+    [self.client publish:fff
+               toChannel:@"Bus_Stop_A"
+          storeInHistory:YES
+          withCompletion:^(PNPublishStatus *status) {
+              if (!status.isError) {
+                  NSLog(@"publish successfully");
+                  // Message successfully published to specified channel.
+              }
+              else
+              {
+                  NSLog(@"Fail");
+              }
+          }];
+    NSLog(@"GO Send A");
+}
+
+- (IBAction)btnRouteBClicked:(id)sender {
+    [self.client subscribeToChannels:@[@"Bus_Stop_A"] withPresence:YES];
+    [self.client subscribeToChannels:@[@"Bus_Stop_B"] withPresence:YES];
+    endStop = @"Bus_Stop_B";
+}
+
+- (IBAction)btnRouteCClicked:(id)sender {
+    [self.client subscribeToChannels:@[@"Bus_Stop_A"] withPresence:YES];
+    [self.client subscribeToChannels:@[@"Bus_Stop_C"] withPresence:YES];
+    endStop = @"Bus_Stop_C";
 }
 
 - (void)vibratePhone;
@@ -88,4 +164,126 @@
         AudioServicesPlayAlertSound(1105);
     }
 }
+
+#pragma mark - PubNub 
+// Handle new message from one of channels on which client has been subscribed.
+- (void)client:(PubNub *)client didReceiveMessage:(PNMessageResult *)message {
+    
+    // Handle new message stored in message.data.message
+    if (![message.data.channel isEqualToString:message.data.subscription]) {
+        // Message has been received on channel group stored in message.data.subscription.
+    }
+    else {
+        
+        // Message has been received on channel stored in message.data.channel.
+    }
+    
+    NSLog(@"Received message: %@ on channel %@ at %@", message.data.message,
+          message.data.channel, message.data.timetoken);
+    
+    NSLog(@"dataMessageClass: %@", [message.data.message class]);
+    
+    NSData *objectData = [message.data.message dataUsingEncoding:NSUTF8StringEncoding];
+    NSError *error = nil;
+    NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:objectData
+                                                             options:NSJSONReadingMutableContainers
+                                                               error:&error];
+    
+    NSLog(@"channel: %@", message.data.channel);
+    NSString *actionlValue = [jsonDict objectForKey:@"action"];
+    NSLog(@"action: %d", [actionlValue intValue]);
+    
+    
+    if ([actionlValue intValue] == 0)
+    {
+        if ([beginStop isEqualToString:message.data.channel])
+        {
+            // notify coming bus
+        }
+        else
+        {
+            // notify stopping bus
+        }
+        
+        //Virbration
+//        [self vibratePhone];
+    }
+    [self vibratePhone];
+}
+
+// Handle subscription status change.
+- (void)client:(PubNub *)client didReceiveStatus:(PNStatus *)status {
+    
+    if (status.operation == PNSubscribeOperation) {
+        
+        // Check whether received information about successful subscription or restore.
+        if (status.category == PNConnectedCategory || status.category == PNReconnectedCategory) {
+            
+            // Status object for those categories can be casted to `PNSubscribeStatus` for use below.
+            PNSubscribeStatus *subscribeStatus = (PNSubscribeStatus *)status;
+            if (subscribeStatus.category == PNConnectedCategory) {
+                
+                // This is expected for a subscribe, this means there is no error or issue whatsoever.
+            }
+            else {
+                
+                /**
+                 This usually occurs if subscribe temporarily fails but reconnects. This means there was
+                 an error but there is no longer any issue.
+                 */
+            }
+        }
+        else if (status.category == PNUnexpectedDisconnectCategory) {
+            
+            /**
+             This is usually an issue with the internet connection, this is an error, handle
+             appropriately retry will be called automatically.
+             */
+        }
+        // Looks like some kind of issues happened while client tried to subscribe or disconnected from
+        // network.
+        else {
+            
+            PNErrorStatus *errorStatus = (PNErrorStatus *)status;
+            if (errorStatus.category == PNAccessDeniedCategory) {
+                
+                /**
+                 This means that PAM does allow this client to subscribe to this channel and channel group
+                 configuration. This is another explicit error.
+                 */
+            }
+            else {
+                
+                /**
+                 More errors can be directly specified by creating explicit cases for other error categories
+                 of `PNStatusCategory` such as: `PNDecryptionErrorCategory`,
+                 `PNMalformedFilterExpressionCategory`, `PNMalformedResponseCategory`, `PNTimeoutCategory`
+                 or `PNNetworkIssuesCategory`
+                 */
+            }
+        }
+    }
+    else if (status.operation == PNUnsubscribeOperation) {
+        
+        if (status.category == PNDisconnectedCategory) {
+            
+            /**
+             This is the expected category for an unsubscribe. This means there was no error in unsubscribing
+             from everything.
+             */
+        }
+    }
+    else if (status.operation == PNHeartbeatOperation) {
+        
+        /**
+         Heartbeat operations can in fact have errors, so it is important to check first for an error.
+         For more information on how to configure heartbeat notifications through the status
+         PNObjectEventListener callback, consult http://www.pubnub.com/docs/ios-objective-c/api-reference#configuration_basic_usage
+         */
+        
+        if (!status.isError) { /* Heartbeat operation was successful. */ }
+        else { /* There was an error with the heartbeat operation, handle here. */ }
+    }
+}
+
 @end
